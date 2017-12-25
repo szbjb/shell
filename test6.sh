f4acf64117fffa96c01031de1834a6ee3a766375
szbjb <846492120@qq.com>
2017-12-25 18:19:42 +0800

    shell的数值运算

--- test6.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#copy the /usr/bin directory listing to a log file
-#将/ usr / bin目录列表复制到日志文件中
-today=$(date +%y+%m+%d)
-ls /usr/bin -al > log.$today
-echo "operation success！"
---
+++ test8.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#使用方括号 简单计算
+var1=100
+var2=50
+var3=45
+var4=$[$var1 * ($var2 - $var3)]
+echo The final result is $var4
